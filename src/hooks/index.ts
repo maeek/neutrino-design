0f31e9e1df0bc43611eda5fa08d809b1dc9b8284
maeek <maciek299.ms@gmail.com>
2020-07-04 22:32:56 +0200

    Added exports for hooks and its methods

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,17 @@
 import emoji from './emoji';
+import avatar from './avatar';
+import connectivity from './connectivity';
 
 export { emojiMapper } from './emoji';
 
+export {
+  computeAvatar,
+  getInitials,
+  DEFAULT_AVATAR_OPTIONS as defaultOptions
+} from './avatar';
+
 export default {
-  emoji
+  emoji,
+  avatar,
+  connectivity
 };
