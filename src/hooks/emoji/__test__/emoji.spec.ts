94e49e13fcf6ccb004c751e4b69f1669cfca101a
maeek <maciek299.ms@gmail.com>
2020-10-08 21:17:53 +0200

    Updated tests
    not passing now

---
+++ src/hooks/emoji/__test__/emoji.spec.ts
@@ -0,0 +1,32 @@
+import { emojiMapper } from '..';
+
+describe.skip('emoijMapper', () => {
+  const exampleString = 'I 🧡 Twemoji! 🥳';
+  const exampleArray = [
+    'I 🧡 Twemoji! 🥳',
+    'I 🧡 Twemoji! 🥳',
+    'I 🧡 Twemoji! 🥳'
+  ];
+
+  /**
+   * TODO: Add proper expected results
+   */
+  const expectedObj = {
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/',
+    indices: [0, 1],
+    text: '',
+    type: 'emoji'
+  };
+
+  it('Parse string', () => {
+    expect(emojiMapper(exampleString)).toEqual([expectedObj]);
+  });
+
+  it('Parse array of strings', () => {
+    expect(emojiMapper(exampleArray)).toEqual([
+      expectedObj,
+      expectedObj,
+      expectedObj
+    ]);
+  });
+});
