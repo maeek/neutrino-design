d2f870e6eff2554b44a1063c945d5adb5c290d40
maeek <maciek299.ms@gmail.com>
2021-02-21 23:30:49 +0100

    Added typography heading

---
+++ src/components/atoms/typography/heading/Heading.tsx
@@ -0,0 +1,55 @@
+import classnames from 'classnames';
+import { FC, ReactNode } from 'react';
+import './heading.scss';
+
+export interface HeadingProps {
+  children: ReactNode;
+  className?: string;
+  level?: 1 | 2 | 3 | 4 | 5;
+}
+
+export const Heading: FC<HeadingProps> = (props) => {
+  const {
+    children,
+    className,
+    level = 1,
+    ...rest
+  } = props;
+
+  const classes = classnames('ne-typo', 'ne-typo-heading', `ne-typo-heading-level-${level}`, className);
+
+  switch (level) {
+  case 2:
+    return (
+      <h2 className={classes} {...rest}>
+        {children}
+      </h2>
+    );
+  case 3:
+    return (
+      <h3 className={classes} {...rest}>
+        {children}
+      </h3>
+    );
+  case 4:
+    return (
+      <h4 className={classes} {...rest}>
+        {children}
+      </h4>
+    );
+  case 5:
+    return (
+      <h5 className={classes} {...rest}>
+        {children}
+      </h5>
+    );
+  default:
+    return (
+      <h1 className={classes} {...rest}>
+        {children}
+      </h1>
+    );
+  }
+};
+
+export default Heading;
