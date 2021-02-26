e64c605c0573f89456957b698f0465d4e85ca58e
maeek <maciek299.ms@gmail.com>
2021-02-26 19:20:59 +0100

    style: Updated build output appereance

--- scripts/build.js
+++ scripts/build.js
@@ -2,35 +2,56 @@
 const path = require('path');
 const config = require('./config');
 const util = require('./common');
+const childProc = require('child_process');
 const { processScss } = require('./processors/sassProcessor');
 const { processTypescript } = require('./processors/tsProcessor');
 
+
+const genTypes = () => {
+  console.time('[\x1b[32m✓\x1b[0m] Generated types in');
+  console.log('Generating typescript files...');
+
+  console.log(
+    childProc.execSync('npm run compile:types', {
+      cwd: path.resolve(__dirname, '..')
+    }).toString()
+  );
+  
+  console.timeEnd('[\x1b[32m✓\x1b[0m] Generated types in');
+};
+
 const loadedModules = util.getEntries(path.resolve(__dirname, '..', config.entryFolder));
 
-console.time('[🟢] Compilation ended successfuly in');
+genTypes();
+
+console.log();
+console.log();
+console.log('Compiling files...');
+console.log();
+console.time('[\x1b[32m✓\x1b[0m] Compilation ended successfuly in');
 
 loadedModules.forEach((mod) => {
   try {
-    console.time(`[🟢] [${mod.meta.type}][${mod.meta.name}] Compilation successful, time elapsed`);
+    console.time(`[\x1b[32m✓\x1b[0m] [\x1b[2m${mod.meta.type}\x1b[0m][${mod.meta.name}] Compilation successful, time elapsed`);
 
     if (mod.meta.typescriptCount > 0) {
-      console.log(`[🟡] [${mod.meta.type}][${mod.meta.name}] Compiling ${mod.meta.typescriptCount} typescript file${mod.meta.typescriptCount > 1 ? 's' : ''}`);
+      console.log(`[\x1b[32m✓\x1b[0m] [\x1b[2m${mod.meta.type}\x1b[0m][${mod.meta.name}] Compiling ${mod.meta.typescriptCount} typescript file${mod.meta.typescriptCount > 1 ? 's' : ''}`);
       processTypescript(mod.rootPath, mod.files.typescript);
     }
     
     if (mod.meta.scssCount > 0) {
-      console.log(`[🟡] [${mod.meta.type}][${mod.meta.name}] Compiling ${mod.meta.scssCount} scss file${mod.meta.scssCount > 1 ? 's' : ''}`);
+      console.log(`[\x1b[32m✓\x1b[0m] [\x1b[2m${mod.meta.type}\x1b[0m][${mod.meta.name}] Compiling ${mod.meta.scssCount} scss file${mod.meta.scssCount > 1 ? 's' : ''}`);
       processScss(mod.rootPath);
     }
 
-    console.timeEnd(`[🟢] [${mod.meta.type}][${mod.meta.name}] Compilation successful, time elapsed`);
+    console.timeEnd(`[\x1b[32m✓\x1b[0m] [\x1b[2m${mod.meta.type}\x1b[0m][${mod.meta.name}] Compilation successful, time elapsed`);
   } catch (e) {
-    console.log(`[🔴] [${mod.meta.type}][${mod.meta.name}] Compilation failure`);
+    console.log(`[\x1b[31m✕\x1b[0m] [\x1b[2m${mod.meta.type}\x1b[0m][${mod.meta.name}] Compilation failure`);
     console.log();
     console.error(e);
   }
 });
 
-console.log();
-console.timeEnd('[🟢] Compilation ended successfuly in');
+console.log('[\x1b[32m✓\x1b[0m]');
+console.timeEnd('[\x1b[32m✓\x1b[0m] Compilation ended successfuly in');
 console.log();
