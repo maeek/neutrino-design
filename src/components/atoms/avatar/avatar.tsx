0d4d788293c44f3243825e782f40f996a63a2841
maeek <maciek299.ms@gmail.com>
2021-03-19 15:15:50 +0100

    refactor: Avatar styles

--- src/components/atoms/avatar/avatar.tsx
+++ src/components/atoms/avatar/avatar.tsx
@@ -1,20 +1,55 @@
 import { FC, ReactNode, memo } from 'react';
 import classnames from 'classnames';
-import './avatar.scss';
-import { ImageContainer } from '../image/image';
+import ImageContainer from '../image/image';
+import './styles/avatar.scss';
 
 export type AvatarTypes = 'round' | 'rounded' | 'square';
 export type AvatarSizes = 'small' | 'medium' | 'large' | 'larger' | 'extra-large';
 
 export interface AvatarProps {
+  /**
+   * Additional classname for the wrapper div
+   */
   className?: string;
+  /**
+   * Image source url that will be displayed
+   */
   src?: string;
+  /**
+   * Alt parameter that will be showed when image will fail to load
+   */
   name?: string;
+  /**
+   * Children that will be rendered below the avatar
+   */
   children?: ReactNode | ((url: string) => ReactNode);
+  /**
+   * Type of avatar appereance, defaults to 'round'
+   */
   type?: 'round' | 'rounded' | 'square';
+  /**
+   * Size of the avatar
+   *  - small 25x25
+   *  - medium 35x35
+   *  - large 50x50
+   *  - larger 100x100
+   *  - extra-large 200x200
+   * 
+   * Defaults to 'medium'
+   */
   size?: 'small' | 'medium' | 'large' | 'larger' | 'extra-large';
+  /**
+   * Should the avatar be selectable by keyboard navigation
+   */
   selectable?: boolean;
+  /**
+   * tabIndex of the avatar element
+   */
   tabIndex?: number;
+  /**
+   * Loader that will be passed to ImageContainer and displayed when image is Loading
+   * No loader will be displayed if omitted
+   */
   loader?: ReactNode;
   [key: string]: any;
 }
