e5d28decc5b598be1040d02da2beb79f4ed1dcfe
maeek <maciej@suchanecki.me>
2021-07-13 18:12:16 +0200

    storybook: Added show/hide context-menu example

--- src/components/molecules/context-menu/Context-menu.stories.tsx
+++ src/components/molecules/context-menu/Context-menu.stories.tsx
@@ -1,5 +1,6 @@
 import { ShareRounded, VisibilityRounded } from '@material-ui/icons';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { useState } from 'react';
 
 import ContextMenuComponent, { ContextMenuProps } from './Menu';
 
@@ -14,6 +15,27 @@ const Template: Story<ContextMenuProps> = (args) => (
 
 export const Menu = Template.bind({});
 Menu.args = {
+  items: [
+    { text: 'Join' },
+    { text: 'Share', icon: <ShareRounded /> },
+    { text: 'Delete Channel' },
+    { text: 'View Channel Properties', icon: <VisibilityRounded /> }
+  ]
+};
+
+const TemplateShowHide: Story<ContextMenuProps> = (args) => {
+  const [ isShown, show ] = useState(false);
+
+  return (
+    <>
+      <button onClick={() =>show((prev) => !prev)}>{isShown ? 'Hide' : 'Show'}</button>
+      {isShown ? <ContextMenuComponent {...args} /> : null }
+    </>
+  );
+};
+
+export const ShowHideMenu = TemplateShowHide.bind({});
+ShowHideMenu.args = {
   items: [
     { text: 'Join' },
     { text: 'Share', icon: <ShareRounded /> },
