f9dc58b0431f894679c4df6ac6965c08a503dda8
maeek <maciej@suchanecki.me>
2021-08-24 15:58:12 +0200

    build: Cleaning publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,6 +3,7 @@ const path = require('path');
 const util = require('./common');
 const childProc = require('child_process');
 const chalk = require('chalk');
+
 const pkgPath = path.resolve(__dirname, '..', 'package.json');
 
 util.copyFiles([ pkgPath ]);
