799769626feae607372c5d6f199b0e055eaeb440
maeek <maciej@suchanecki.me>
2022-03-13 09:32:04 +0100

    style: Updated storybook theme

--- .storybook/neutrino-theme.js
+++ .storybook/neutrino-theme.js
@@ -4,5 +4,15 @@ export default create({
   base: 'dark',
   brandTitle: 'Neutrino Design',
   brandUrl: 'https://github.com/maeek/neutrino-design.git',
-  brandImage: ''
+  brandImage: '',
+  appBg: '#22262b',
+  appContentBg: '#25292f',
+  colorPrimary: '#267ce0',
+  colorSecondary: '#267ce0',
+  appBorderColor: '#25282e',
+  barBg: '#202328',
+  inputBg: '#343a43',
+  textColor: '#fff',
+  textMutedColor: '#808080',
+  inputBorderRadius: '0.3rem'
 });
