6a33ec3a63a26242899e37344f5a4f8e40c7fb7d
maeek <maciej@suchanecki.me>
2022-03-26 18:49:30 +0100

    feat: Atoms - chip

---
+++ src/components/atoms/chip/Chip.tsx
@@ -0,0 +1,68 @@
+import { CSSProperties, MouseEventHandler, ReactNode } from 'react';
+import classNames from 'classnames';
+import './chip.scss';
+import { CloseRounded } from '@material-ui/icons';
+
+export interface ChipProps {
+  icon?: ReactNode;
+  children?: ReactNode;
+  color?: 'blue' | 'red' | 'green' | 'yellow' | 'purple';
+  deletable?: boolean;
+  onDelete?: MouseEventHandler;
+  onClick?: MouseEventHandler;
+  disabled?: boolean;
+  type?: 'round' | 'rounded' | 'square';
+  style?: CSSProperties;
+  className?: string;
+}
+
+export const Chip = (props: ChipProps) => {
+  const {
+    type = 'rounded',
+    icon,
+    color,
+    children,
+    deletable = false,
+    onDelete,
+    onClick,
+    style,
+    className,
+    disabled = false
+  } = props;
+
+  const classes = classNames(
+    'ne-chip',
+    `ne-chip--${type}`,
+    `ne-chip--color-${color}`,
+    {
+      'ne-chip--disabled': disabled,
+      'ne-chip--deletable': deletable,
+      'ne-chip--icon': !!icon
+    },
+    className
+  );
+
+  const onDeleteHandler: MouseEventHandler = (e) => {
+    e.stopPropagation();
+
+    if (onDelete && !disabled && deletable) {
+      onDelete(e);
+    }
+  };
+
+  return (
+    <div className={classes} style={style} tabIndex={0} onClick={onClick}>
+      {icon && <span className='ne-chip-icon'>{icon}</span>}
+      <span className='ne-chip-content'>{children}</span>
+      {deletable && (
+        <button
+          className="ne-chip-delete"
+          onClick={onDeleteHandler}
+          disabled={disabled}
+        >
+          <CloseRounded />
+        </button>
+      )}
+    </div>
+  );
+};
